909444e517e6914dd4c085cdfb4dc0dcc2105536
thalesleite <thalesaleite@gmail.com>
2020-04-01 10:41:46 +0100

    added prod and dev url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const devServer = 'http://localhost:3333';
+const prodServer = 'https://app-be-hero.herokuapp.com';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: prodServer,
 });
 
 export default api;
